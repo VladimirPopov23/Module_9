596e7c4d589da9e4e116458a9bed3bed73808d76
VladimirPopov23 <volodyaklever@gmail.com>
2024-11-20 08:33:46 +0300

    module_9_3.py
    20.11.2024 Домашнее задание по теме "Генераторные сборки"

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# module_9_2.py
-# 20.11.2024 Домашнее задание по теме "Списковые, словарные сборки"
+# module_9_3.py
+# 20.11.2024 Домашнее задание по теме "Генераторные сборки"
 
-first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
-second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = [len(x) for x in first_strings if (len(x) >= 5)]
-second_result = [(x, y) for x in first_strings for y in second_strings if (len(x) == len(y))]
-third_result = {x: len(x) for x in first_strings + second_strings if (len(x) % 2) == 0}
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+second_result = (len(first[x]) == len(second[x]) for x in range(len(first)))
 
-print(first_result)
-print(second_result)
-print(third_result)
+print(list(first_result))
+print(list(second_result))
