0acdee77581e6b8b873e4be73fdaab91729f03f7
VladimirPopov23 <volodyaklever@gmail.com>
2024-11-24 00:37:15 +0300

    module_9_5.py
    24.11.2024 Задача "Range - это просто"

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-# module_9_4.py
-# 23.11.2024 Задача "Функциональное разнообразие"
+# module_9_5.py
+# 24.11.2024 Задача "Range - это просто"
 
-from pprint import pprint
-from random import choice
+class StepValueError(ValueError):
+    pass
 
-#  Задание 1. Lambda-функция:
-first = 'Мама мыла раму'
-second = 'Рамена мало было'
-print(list(map(lambda x, y: x == y, first, second)))
+class Iterator:
+    def __init__(self, start, stop, step = 1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
 
+    def __iter__ (self):
+        self.pointer = self.start
+        return self
 
-# Задание 2. Замыкание:
-def get_advanced_writer(file_name):
-    def write_everything(*data_set):
-        with open(file_name, 'w', encoding='utf-8') as file:
-            for i in data_set:
-                file.write(f'{i}\n')
-    return write_everything
-
-
-write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
-
-
-#  Задание 3. Метод __call__:
-class MysticBall:
-    def __init__(self, *words):
-        self.words = words
-
-    def __call__(self):
-        return choice(self.words)
+    def __next__ (self):
+        self.pointer += self.step
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration()
+        return self.pointer
 
 
-first_ball = MysticBall('Да', 'Нет', 'Наверное')
-print(first_ball())
-print(first_ball())
-print(first_ball())
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1,-1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
