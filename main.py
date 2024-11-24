64cc4b8784d327bc5c1e28339fd0cfbcfeb53b76
VladimirPopov23 <volodyaklever@gmail.com>
2024-11-24 23:24:54 +0300

    module_9_7.py
    24.11.2024 Задание: Декораторы в Python

--- main.py
+++ main.py
@@ -7,17 +7,39 @@ def is_prime(func):
 # для проверки на простоту необходимо убедится что число больше 0 и не делится ни на что, кроме 1 и самого себя
         if res > 1:
             for j in range(2, res): # в диапазоне от 2 до числа (не включительно)
-                if res % j != 0: # если остаток от деления числа не равен 0, то число простое
-                    print('Простое')
-                else: # иначе число составное
+                if res % j == 0: # если остаток от деления числа не равен 0, то число составное
                     print('Составное')
-                return res # возвращает значение из ф-ции sum_three
-    return wrapper # возвращает wrapper
+                    break
+            else: # иначе число простое
+                print('Простое')
+        return res  # возвращаем значение из ф-ции sum_three
+    return wrapper  # возвращаем wrapper
 
 @is_prime
 def sum_three(a, b, c):
     sum_ = a + b + c # сумма передаваемых чисел
     return sum_
 
+result = sum_three(0, 1, 1)
+print(result)
+result = sum_three(1, 1, 1)
+print(result)
+result = sum_three(1, 2, 1)
+print(result)
+result = sum_three(1, 2, 2)
+print(result)
+result = sum_three(1, 2, 3)
+print(result)
+result = sum_three(1, 2, 4)
+print(result)
+result = sum_three(1, 2, 5)
+print(result)
+result = sum_three(1, 2, 6)
+print(result)
+result = sum_three(1, 2, 7)
+print(result)
 result = sum_three(2, 3, 6)
 print(result)
+result = sum_three(2, 4, 6)
+print(result)
+
